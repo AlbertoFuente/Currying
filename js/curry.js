9be4aa220ce221c2132d25ec31bfaf306436d76a
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-08-02 20:20:14 +0200

    Working in Currying samples

---
+++ js/curry.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+    /* First we need the curry function
+        expects a function as its argument
+        */
+    function curry(fn) {
+        // Variable with the arguments length
+        argsLength = fn.length;
+
+        return (function resolver() {
+            // variable with the copy of the arguments received
+            var argsReceived = Array.prototype.slice.call(arguments);
+            // return function
+            return function() {
+                // argsReceived copy
+                var localArgs = argsReceived.slice();
+                Array.prototype.push.apply(localArgs, arguments);
+                // compare the length of arguments we’ve received with the argsLength of our curried function
+                var next = localArgs.length >= argsLength ? fn : resolver;
+                return next.apply(null, localArgs);
+            };
+        }());
+    }
+}());
