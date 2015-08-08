60235361cdda87056d2e8c931b60692a01d3d146
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-08-08 10:21:51 +0200

    Created gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    karma = require('gulp-karma'),
+    testFiles = ['js/curry.js', 'test/spec.js'];
+
+gulp.task('tests', function() {
+    gulp.src(testFiles)
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'run'
+        }))
+        .on('error', function(err) {
+            throw err;
+        });
+});
+
+gulp.task('watch', function() {
+    'use strict';
+
+    gulp.watch([
+        'js/curry.js', 'test/spec.js'
+    ], function() {
+        gulp.start('tests');
+    });
+});
+
+gulp.task('default', function() {
+    gulp.start('tests');
+});
