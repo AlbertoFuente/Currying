c4a2bed14ea4c333d1cc8061a937d03a90e7e519
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-08-09 20:39:40 +0200

    Working on unit tests

--- test/spec.js
+++ test/spec.js
@@ -1,3 +1,9 @@
 describe('Testing curry functions', function() {
-    var curry = curry;
+    it('Our function returns simple multiplication', function() {
+        function volume(l, w, h) {
+            return l * w * h;
+        }
+        var curried = curryFn.curry(volume);
+        expect(curried(1)(2)(3)).toBe(6);
+    });
 });
